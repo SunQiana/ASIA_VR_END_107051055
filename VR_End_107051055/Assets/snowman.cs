0ce48f9a65c9a78be0f2a945330450e2a3076a31
SunQiana <71679899+SunQiana@users.noreply.github.com>
2021-01-17 19:17:22 +0800

    2021/1/17

---
+++ VR_End_107051055/Assets/snowman.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class snowman : MonoBehaviour
+{
+    public static int Score;
+    public Text Scoreborad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //如果雪球碰撞
+        if (other.tag == "snowball") 
+        {
+            //雪人bang不見
+            Destroy(this.gameObject);
+            Score = Score + 1;
+        }
+    }
+
+    void Update()
+    {
+        Scoreborad.text = Score.ToString();
+    }
+}
